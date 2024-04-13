3286e15369f3985d21ab87d0ea516ba6a8934131
aerial-301 <75935539+aerial-301@users.noreply.github.com>
2024-04-13 21:52:53 +0300

    Fix type defs

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -2,21 +2,25 @@ import { Unit } from './scenes/unit'
 /**
  * @typedef Team
  * @type {object}
+ * @prop {string} name
  * @prop {Number} x
  * @prop {Number} y
  * @prop {Array<Unit>} units
  * @prop {Team} enemy
  * @prop {Number} score
+ * @prop {Number} dmgColor
+ * @prop {string} bar
  */
 
 /**
  * @typedef UnitType
  * @type {object}
  * @prop {string} name
- * @prop {string} png
+ * @prop {{player: string, other: string}} png
  * @prop {Number} health
  * @prop {Number} damage
  * @prop {Number} range
  * @prop {Number} fireRate
  * @prop {Number} moveSpeed
+
  */
